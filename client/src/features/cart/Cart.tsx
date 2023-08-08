1a2211703467205c82a179b8668d0b6e3a2b77ee
Bui Cong Thuan <thuanbui2611@gmail.com>
2023-08-08 17:32:51 +0700

    r

--- client/src/features/cart/Cart.tsx
+++ client/src/features/cart/Cart.tsx
@@ -266,7 +266,7 @@ export default function Cart() {
                 </div>
               </div>
               <div className="flex-wrap items-center hidden mb-6 -mx-4 md:flex md:mb-8">
-                <div className="w-full px-4 mb-6 md:w-4/6 lg:w-6/12 md:mb-0 text-center">
+                <div className="w-full px-4 mb-6 md:w-1/2  md:mb-0 text-center">
                   <h2 className="font-bold text-gray-500 mr-12 ">
                     Product name
                   </h2>
@@ -280,19 +280,15 @@ export default function Cart() {
                 <div className="w-auto px-4 md:w-1/6 lg:w-2/12 ">
                   <h2 className="font-bold text-gray-500 ml-5 ">Price</h2>
                 </div>
-
-                <div className="w-auto px-4 text-right md:w-1/6 lg:w-2/12 ">
-                  <h2 className="font-bold text-gray-500 "> Subtotal</h2>
-                </div>
               </div>
               <div className=" py-4 mb-8 border-t border-b border-gray-200 scrollbar overflow-y-auto overflow-x-hidden max-h-[100vw] md:max-h-[21vw] ">
                 <div className="flex flex-wrap items-center mb-6 -mx-4 md:mb-8">
                   <div className="w-auto px-4 md:w-1/12">
                     <input type="checkbox" className="mr-2" />
                   </div>
-                  <div className="w-full px-4 mb-6 md:w-4/6 lg:w-6/12 md:mb-0">
+                  <div className="w-full px-4 mb-6 md:w-1/2 lg:w-2/5 md:mb-0">
                     <div className="flex flex-wrap items-center -mx-4">
-                      <div className="w-full px-4 mb-3 md:w-1/3">
+                      <div className="w-full mb-3 md:w-fit">
                         <div className="w-full h-96 md:h-24 md:w-24">
                           <img
                             src="https://i.postimg.cc/CLWkvq6f/pexels-markus-spiske-1002638.jpg"
@@ -301,7 +297,7 @@ export default function Cart() {
                           />
                         </div>
                       </div>
-                      <div className="w-2/3 px-4">
+                      <div className="w-2/3 pl-4">
                         <h2 className="mb-2 text-xl font-bold ">DSL Camera</h2>
                         <p className="text-gray-500  ">Picture frame</p>
                       </div>
@@ -309,41 +305,7 @@ export default function Cart() {
                   </div>
 
                   <div className="hidden px-4 lg:block lg:w-2/12">
-                    <p className="text-lg font-bold text-blue-500 ">$100.00</p>
-                  </div>
-
-                  <div className="w-auto px-4 md:w-1/6 lg:w-2/12 ">
-                    <div className="inline-flex items-center px-4 font-semibold text-gray-500 border border-gray-200 rounded-md dark:border-gray-700 ">
-                      <button className="py-2 hover:text-gray-700 ">
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          width="16"
-                          height="16"
-                          fill="currentColor"
-                          className="bi bi-dash"
-                          viewBox="0 0 16 16"
-                        >
-                          <path d="M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8z" />
-                        </svg>
-                      </button>
-                      <input
-                        type="number"
-                        className="w-12 px-2 py-4 text-center border-0 rounded-md dark:bg-gray-800 bg-gray-50  md:text-right"
-                        placeholder="1"
-                      />
-                      <button className="py-2 hover:text-gray-700 ">
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          width="16"
-                          height="16"
-                          fill="currentColor"
-                          className="bi bi-plus"
-                          viewBox="0 0 16 16"
-                        >
-                          <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" />
-                        </svg>
-                      </button>
-                    </div>
+                    <p className="text-lg font-bold text-black">65L1-24084</p>
                   </div>
 
                   <div className="w-auto px-4 text-right md:w-1/6 lg:w-2/12 ">
