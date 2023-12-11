784b29162bf252cc5dd07029376466ff638e0055
Bui Cong Thuan <thuanbui2611@gmail.com>
2023-12-11 22:43:39 +0700

    fix

--- client/src/app/components/BlogRelatedCard.tsx
+++ client/src/app/components/BlogRelatedCard.tsx
@@ -8,16 +8,16 @@ interface Props {
 export default function BlogRelatedCard({ blogRelated }: Props) {
   return (
     <>
-      <div className="flex flex-col items-start col-span-12 overflow-hidden shadow-sm rounded-xl md:col-span-6 lg:col-span-4">
-        <Link
-          to={`/blog/${blogRelated.id}`}
-          className="block w-full h-full transition duration-200 ease-out transform hover:scale-110"
-        >
+      <Link
+        to={`/blog/${blogRelated.id}`}
+        className="flex flex-col items-start col-span-12 overflow-hidden shadow-sm rounded-xl md:col-span-6 lg:col-span-4 hover:brightness-90"
+      >
+        <div className="block w-full h-full transition duration-200 ease-out transform hover:scale-110">
           <img
             className="object-cover w-full shadow-sm h-full"
             src={blogRelated.image.imageUrl}
           />
-        </Link>
+        </div>
         <div className="relative flex flex-col items-start px-6 bg-white border border-t-0 border-gray-200 py-7 rounded-b-2xl">
           <div className="bg-gradient-to-r from-[#FF6003] to-[#FF7E06] absolute top-0 -mt-3 flex items-center px-3 py-1.5 leading-none w-auto rounded-full text-xs font-medium uppercase text-white">
             <span>{blogRelated.category.name}</span>
@@ -32,7 +32,7 @@ export default function BlogRelatedCard({ blogRelated }: Props) {
             {blogRelated.shortDescription}
           </p>
         </div>
-      </div>
+      </Link>
     </>
   );
 }
