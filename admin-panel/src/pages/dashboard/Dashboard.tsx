5eca0d37efb69e9faabd587451b803270bcb9ca7
Bui Cong Thuan <thuanbui2611@gmail.com>
2023-12-19 14:14:03 +0700

    add folder

---
+++ admin-panel/src/pages/dashboard/Dashboard.tsx
@@ -0,0 +1,77 @@
+import { ChangeEvent, useState } from "react";
+import { recognizeIdentityCard } from "../../app/utils/IDRecognition";
+import TotalViewsCard from "../../app/components/TotalViewsCard";
+import TotalProfitsCard from "../../app/components/TotalProfitsCard";
+import TotalProductsCard from "../../app/components/TotalProductsCard";
+import TotalUsersCard from "../../app/components/TotalUsersCard";
+import TableTopLessee from "../../app/components/TableTopLessee";
+import TableTopLessor from "../../app/components/TableTopLessor";
+import ChartVistorAnalyze from "../../app/components/ChartVistorAnalyze";
+import ChartTotalRevenue from "../../app/components/ChartTotalRevenue";
+import { useAppSelector } from "../../app/store/ConfigureStore";
+
+const Dashboard = () => {
+  const [ipAddress, setIPAddress] = useState("");
+  const [country, setCountry] = useState("");
+  const [city, setCity] = useState("");
+  // useEffect(() => {
+  //   const timer = setTimeout(() => {
+  //     fetch(
+  //       "https://ipgeolocation.abstractapi.com/v1/?api_key=c886e37f754842e5b6d4cf0375b326da"
+  //     )
+  //       .then((response) => response.json())
+  //       .then((response) => console.log(response))
+  //       .catch((err) => console.error(err));
+  //   }, 5000);
+  //   return () => {
+  //     clearTimeout(timer);
+  //   };
+  // }, []);
+  const [selectedFile, setSelectedFile] = useState<File | null>(null);
+  //
+  const { user } = useAppSelector((state) => state.account);
+
+  const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+    if (!file) return;
+    setSelectedFile(file);
+  };
+  const handleUpload = async () => {
+    if (!selectedFile) return;
+
+    await recognizeIdentityCard(selectedFile);
+  };
+  //
+  return (
+    <>
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6 xl:grid-cols-4 2xl:gap-7.5">
+        <TotalViewsCard />
+        <TotalProfitsCard />
+        <TotalProductsCard />
+        <TotalUsersCard />
+      </div>
+      {user?.role === "Staff" ? (
+        <div className="text-center text-4xl text-[#FF7E06] font-black mt-10">
+          Welcome to Admin Panel of Motormate
+        </div>
+      ) : (
+        <>
+          <div className="bg-black text-white"></div>
+          <div className="mt-4 grid grid-cols-12 gap-4 md:mt-6 md:gap-6 2xl:mt-7.5 2xl:gap-7.5">
+            {/* <div className="bg-red-600 w-90 h-90">
+          <input type="file" onChange={handleFileChange} />
+          <button onClick={handleUpload}>Submit</button>
+        </div> */}
+            <ChartTotalRevenue />
+            {/* <ChartThree /> */}
+            <TableTopLessee />
+            <TableTopLessor />
+            <ChartVistorAnalyze />
+          </div>
+        </>
+      )}
+    </>
+  );
+};
+
+export default Dashboard;
---
+++ admin-panel/src/pages/dashboard/DashboardSlice.ts
@@ -0,0 +1,193 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import {
+  RevenueInYear,
+  TopLessees,
+  TopLessors,
+  TotalProfit,
+  TotalUsers,
+  TotalVehicles,
+} from "../../app/models/Chart";
+import agent from "../../app/api/agent";
+
+interface FilterState {
+  totalVehicles: TotalVehicles | null;
+  totalVehiclesLoading: boolean;
+  totalUsers: TotalUsers | null;
+  totalUsersLoading: boolean;
+  totalProfits: TotalProfit | null;
+  totalProfitsLoading: boolean;
+  topLessees: TopLessees[];
+  topLesseesLoading: boolean;
+  topLessors: TopLessors[];
+  topLessorsLoading: boolean;
+  revenueInYear: RevenueInYear | null;
+  revenueInYearLoading: boolean;
+}
+
+const initialState: FilterState = {
+  totalVehicles: null,
+  totalVehiclesLoading: false,
+  totalUsers: null,
+  totalUsersLoading: false,
+  totalProfits: null,
+  totalProfitsLoading: false,
+  topLessees: [],
+  topLesseesLoading: false,
+  topLessors: [],
+  topLessorsLoading: false,
+  revenueInYear: null,
+  revenueInYearLoading: false,
+};
+
+export const getTotalVehiclesAsync = createAsyncThunk<TotalVehicles>(
+  "dashboard/getTotalVehiclesAsync",
+  async (_, thunkAPI) => {
+    try {
+      const response = await agent.Chart.totalVehicles();
+      return response;
+    } catch (error) {
+      return thunkAPI.rejectWithValue({ error: (error as any).data });
+    }
+  }
+);
+
+export const getTotalUsersAsync = createAsyncThunk<TotalUsers>(
+  "dashboard/getTotalUsersAsync",
+  async (_, thunkAPI) => {
+    try {
+      const response = await agent.Chart.totalUsers();
+      return response;
+    } catch (error) {
+      return thunkAPI.rejectWithValue({ error: (error as any).data });
+    }
+  }
+);
+
+export const getTotalProfitsAsync = createAsyncThunk<TotalProfit>(
+  "dashboard/getTotalProfitsAsync",
+  async (_, thunkAPI) => {
+    try {
+      const response = await agent.Chart.totalProfit();
+      return response;
+    } catch (error) {
+      return thunkAPI.rejectWithValue({ error: (error as any).data });
+    }
+  }
+);
+
+export const getTopLesseesAsync = createAsyncThunk<TopLessees[]>(
+  "dashboard/getTopLesseesAsync",
+  async (_, thunkAPI) => {
+    try {
+      const response = await agent.Chart.topLessees();
+      return response;
+    } catch (error) {
+      return thunkAPI.rejectWithValue({ error: (error as any).data });
+    }
+  }
+);
+
+export const getTopLessorsAsync = createAsyncThunk<TopLessors[]>(
+  "dashboard/getTopLessorsAsync",
+  async (_, thunkAPI) => {
+    try {
+      const response = await agent.Chart.topLessors();
+      return response;
+    } catch (error) {
+      return thunkAPI.rejectWithValue({ error: (error as any).data });
+    }
+  }
+);
+
+export const getRevenueInYearAsync = createAsyncThunk<RevenueInYear, string>(
+  "dashboard/getRevenueInYearAsync",
+  async (year: string, thunkAPI) => {
+    try {
+      const response = await agent.Chart.revenueInYear(year);
+      return response;
+    } catch (error) {
+      return thunkAPI.rejectWithValue({ error: (error as any).data });
+    }
+  }
+);
+
+export const DashboardSlice = createSlice({
+  name: "dashboard",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(getTotalVehiclesAsync.pending, (state, action) => {
+        state.totalVehiclesLoading = true;
+      })
+      .addCase(getTotalVehiclesAsync.fulfilled, (state, action) => {
+        state.totalVehiclesLoading = false;
+        state.totalVehicles = action.payload;
+      })
+      .addCase(getTotalVehiclesAsync.rejected, (state, action) => {
+        console.log("Get total vehicles fail");
+      });
+
+    builder
+      .addCase(getTotalUsersAsync.pending, (state, action) => {
+        state.totalUsersLoading = true;
+      })
+      .addCase(getTotalUsersAsync.fulfilled, (state, action) => {
+        state.totalUsersLoading = false;
+        state.totalUsers = action.payload;
+      })
+      .addCase(getTotalUsersAsync.rejected, (state, action) => {
+        console.log("Get total users fail");
+      });
+
+    builder
+      .addCase(getTotalProfitsAsync.pending, (state, action) => {
+        state.totalProfitsLoading = true;
+      })
+      .addCase(getTotalProfitsAsync.fulfilled, (state, action) => {
+        state.totalProfitsLoading = false;
+        state.totalProfits = action.payload;
+      })
+      .addCase(getTotalProfitsAsync.rejected, (state, action) => {
+        console.log("Get total profits fail");
+      });
+
+    builder
+      .addCase(getTopLesseesAsync.pending, (state, action) => {
+        state.topLesseesLoading = true;
+      })
+      .addCase(getTopLesseesAsync.fulfilled, (state, action) => {
+        state.topLesseesLoading = false;
+        state.topLessees = action.payload;
+      })
+      .addCase(getTopLesseesAsync.rejected, (state, action) => {
+        console.log("Get top lessees fail");
+      });
+
+    builder
+      .addCase(getTopLessorsAsync.pending, (state, action) => {
+        state.topLessorsLoading = true;
+      })
+      .addCase(getTopLessorsAsync.fulfilled, (state, action) => {
+        state.topLessorsLoading = false;
+        state.topLessors = action.payload;
+      })
+      .addCase(getTopLessorsAsync.rejected, (state, action) => {
+        console.log("Get top lessors fail");
+      });
+
+    builder
+      .addCase(getRevenueInYearAsync.pending, (state, action) => {
+        state.revenueInYearLoading = true;
+      })
+      .addCase(getRevenueInYearAsync.fulfilled, (state, action) => {
+        state.revenueInYearLoading = false;
+        state.revenueInYear = action.payload;
+      })
+      .addCase(getRevenueInYearAsync.rejected, (state, action) => {
+        console.log("Get revenue in year fail");
+      });
+  },
+});
+
+export const {} = DashboardSlice.actions;
